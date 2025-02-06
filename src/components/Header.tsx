fb41e2feeb78a97eef61aba3a7dbd1e8ad934901
ThoamsZ <48622682+ThoamsZ@users.noreply.github.com>
2025-02-07 00:18:11 +0800

    sb

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -113,9 +113,9 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-white/95 backdrop-blur-md z-40 border-b shadow-sm">
+    <header className="bg-white/95 backdrop-blur-md z-40 ">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-3">
-        <div className="flex flex-col gap-3">
+        <div className="flex justify-center items-center gap-3">
           {/* Logo */}
           <div className="flex items-center justify-between">
             <h1 
