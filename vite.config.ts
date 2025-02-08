b76920ebef352c36deb42fc1ef081ed6d9476a0c
ThoamsZ <48622682+ThoamsZ@users.noreply.github.com>
2025-02-09 03:55:20 +0800

    sb

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import history from 'connect-history-api-fallback';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [
     react(),
-    {
-      // 应用 history 中间件以处理前端路由
-      configureServer: (server) => {
-        server.middlewares.use(
-          history({
-            // 设置为 true 以在控制台中打印重定向信息
-            verbose: true,
-            // 设置为 '/' 以匹配所有路径
-            rewrites: [
-              { from: /\/.*/, to: '/index.html' },
-            ],
-          })
-        );
-      },
-    },
-  ],
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-
+}));
